024351bb09390ab792535d706f325017cac756a7
Diogo Rachid <di.rachid@gmail.com>
2026-02-20 17:48:18 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,45 +1,94 @@
-import Home from './pages/Home';
-import BibDashboard from './pages/BibDashboard';
+/**
+ * pages.config.js - Page routing configuration
+ * 
+ * This file is AUTO-GENERATED. Do not add imports or modify PAGES manually.
+ * Pages are auto-registered when you create files in the ./pages/ folder.
+ * 
+ * THE ONLY EDITABLE VALUE: mainPage
+ * This controls which page is the landing page (shown when users visit the app).
+ * 
+ * Example file structure:
+ * 
+ *   import HomePage from './pages/HomePage';
+ *   import Dashboard from './pages/Dashboard';
+ *   import Settings from './pages/Settings';
+ *   
+ *   export const PAGES = {
+ *       "HomePage": HomePage,
+ *       "Dashboard": Dashboard,
+ *       "Settings": Settings,
+ *   }
+ *   
+ *   export const pagesConfig = {
+ *       mainPage: "HomePage",
+ *       Pages: PAGES,
+ *   };
+ * 
+ * Example with Layout (wraps all pages):
+ *
+ *   import Home from './pages/Home';
+ *   import Settings from './pages/Settings';
+ *   import __Layout from './Layout.jsx';
+ *
+ *   export const PAGES = {
+ *       "Home": Home,
+ *       "Settings": Settings,
+ *   }
+ *
+ *   export const pagesConfig = {
+ *       mainPage: "Home",
+ *       Pages: PAGES,
+ *       Layout: __Layout,
+ *   };
+ *
+ * To change the main page from HomePage to Dashboard, use find_replace:
+ *   Old: mainPage: "HomePage",
+ *   New: mainPage: "Dashboard",
+ *
+ * The mainPage value must match a key in the PAGES object exactly.
+ */
 import BibAcervo from './pages/BibAcervo';
-import BibIrmaos from './pages/BibIrmaos';
-import BibEmprestimos from './pages/BibEmprestimos';
-import BibQRCodes from './pages/BibQRCodes';
-import IrmaoEmprestimos from './pages/IrmaoEmprestimos';
-import IrmaoScan from './pages/IrmaoScan';
-import ScanRetirada from './pages/ScanRetirada';
-import ScanDevolucao from './pages/ScanDevolucao';
-import BibLogin from './pages/BibLogin';
-import IrmaoLogin from './pages/IrmaoLogin';
-import BibBibliotecarios from './pages/BibBibliotecarios';
 import BibAcervoDigital from './pages/BibAcervoDigital';
-import IrmaoAcervoDigital from './pages/IrmaoAcervoDigital';
+import BibBibliotecarios from './pages/BibBibliotecarios';
+import BibDashboard from './pages/BibDashboard';
+import BibEmprestimos from './pages/BibEmprestimos';
+import BibIrmaos from './pages/BibIrmaos';
 import BibLogAcessos from './pages/BibLogAcessos';
 import BibLogDownloads from './pages/BibLogDownloads';
+import BibLogin from './pages/BibLogin';
+import BibQRCodes from './pages/BibQRCodes';
 import BibRelatorios from './pages/BibRelatorios';
+import Home from './pages/Home';
 import IrmaoAcervo from './pages/IrmaoAcervo';
+import IrmaoAcervoDigital from './pages/IrmaoAcervoDigital';
+import IrmaoEmprestimos from './pages/IrmaoEmprestimos';
+import IrmaoLogin from './pages/IrmaoLogin';
+import IrmaoScan from './pages/IrmaoScan';
+import ScanDevolucao from './pages/ScanDevolucao';
+import ScanRetirada from './pages/ScanRetirada';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Home": Home,
-    "BibDashboard": BibDashboard,
     "BibAcervo": BibAcervo,
-    "BibIrmaos": BibIrmaos,
-    "BibEmprestimos": BibEmprestimos,
-    "BibQRCodes": BibQRCodes,
-    "IrmaoEmprestimos": IrmaoEmprestimos,
-    "IrmaoScan": IrmaoScan,
-    "ScanRetirada": ScanRetirada,
-    "ScanDevolucao": ScanDevolucao,
-    "BibLogin": BibLogin,
-    "IrmaoLogin": IrmaoLogin,
-    "BibBibliotecarios": BibBibliotecarios,
     "BibAcervoDigital": BibAcervoDigital,
-    "IrmaoAcervoDigital": IrmaoAcervoDigital,
+    "BibBibliotecarios": BibBibliotecarios,
+    "BibDashboard": BibDashboard,
+    "BibEmprestimos": BibEmprestimos,
+    "BibIrmaos": BibIrmaos,
     "BibLogAcessos": BibLogAcessos,
     "BibLogDownloads": BibLogDownloads,
+    "BibLogin": BibLogin,
+    "BibQRCodes": BibQRCodes,
     "BibRelatorios": BibRelatorios,
+    "Home": Home,
     "IrmaoAcervo": IrmaoAcervo,
+    "IrmaoAcervoDigital": IrmaoAcervoDigital,
+    "IrmaoEmprestimos": IrmaoEmprestimos,
+    "IrmaoLogin": IrmaoLogin,
+    "IrmaoScan": IrmaoScan,
+    "ScanDevolucao": ScanDevolucao,
+    "ScanRetirada": ScanRetirada,
 }
 
 export const pagesConfig = {
