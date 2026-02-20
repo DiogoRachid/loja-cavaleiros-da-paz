3df360ba7540bfeb9e9c727902ed50c608103a3a
Diogo Rachid <di.rachid@gmail.com>
2026-02-20 18:05:01 +0000

    File changes

--- src/pages/BibAcervoDigital.jsx
+++ src/pages/BibAcervoDigital.jsx
@@ -33,7 +33,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import AcervoDigitalForm from "@/components/biblioteca/AcervoDigitalForm.jsx";
+import AcervoDigitalForm from "@/components/biblioteca/AcervoDigitalForm.js";
 
 export default function BibAcervoDigital() {
   const [documentos, setDocumentos] = useState([]);
